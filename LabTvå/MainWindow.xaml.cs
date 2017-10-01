f332e77494a4db01a794f7ab2842243ef07e8032
Adam Andersson <adamanderss@gmail.com>
2017-10-01 19:57:40 +0200

    testexempel "hej"

--- LabTvå/MainWindow.xaml.cs
+++ LabTvå/MainWindow.xaml.cs
@@ -20,9 +20,41 @@ namespace LabTvå
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static string name;
+        public static string date;
+        public static string comment;
+        public static string totalInput;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                totalInput = "Namn: " + name + "\nDatum: " + date + "\nKommentar: " + comment;
+                listBox1.Items.Add(totalInput.ToString());
+         
+                this.nameInput.Clear();
+                this.dateInput.Clear();
+                this.commentInput.Clear();
+                this.nameInput.Focus(); 
+        }
+        private void NameInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            name = nameInput.Text;
+        }
+        private void DateInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            date = dateInput.Text;          
+        }
+        private void CommentInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            comment = commentInput.Text;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
         }
     }
 }
